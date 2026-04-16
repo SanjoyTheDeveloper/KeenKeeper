4598e4739d3e68136a5b2a06c2de307413330336
SanjoyTheDeveloper <info.sanjoy.bh@gmail.com>
2026-04-16 23:32:57 +0600

    create json data

--- src/ui/Navber.jsx
+++ src/ui/Navber.jsx
@@ -8,17 +8,16 @@ export function Navber() {
             <div className="mx-auto flex max-w-9xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
                 <div className="flex items-center gap-3">
                     <div className="rounded-3xl  px-4 py-2 text-lg  ">
-                        <span className="font-bold">Keen</span>
-                        <span >Keeper</span>
+                        <img src="" alt="" />
                          
                     </div>
                     
                 </div>
                 <nav className="flex items-center gap-2">
                     
-                   <span><RiHome2Line /></span><button className="rounded-ful text-sm mr-3 font-medium text-slate-700 transition hover:bg-slate-100">Home</button>
-                    <span><IoTimeOutline /></span><button className="rounded-full text-sm mr-3 font-medium text-slate-700 transition hover:bg-slate-100">Timeline</button>
-                    <span><ImStatsDots /></span><button className="rounded-full  text-sm font-medium text-slate-700 transition hover:bg-slate-100">Stats</button>
+                   <span className="flex bg-green-500 py-2 px-1 rounded-sm items-center gap-2"><RiHome2Line /><button className="rounded-ful text-sm mr-3 font-medium text-slate-700 transition">Home</button></span>
+                    <span className="flex items-center gap-2"><IoTimeOutline /><button className="rounded-full text-sm mr-3 font-medium text-slate-700 transition ">Timeline</button></span>
+                    <span className="flex items-center gap-2"><ImStatsDots /><button className="rounded-full  text-sm font-medium text-slate-700 transition ">Stats</button></span>
                 </nav>
             </div>
         </header>
